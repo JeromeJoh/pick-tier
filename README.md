35b3285a4a3ed91d25cdf082392c8b1cbb49c1aa
JeromeJoh <jerome.yzhao@foxmail.com>
2026-01-08 23:41:13 +0800

    feat: first demo with vibe coding

--- README.md
+++ README.md
@@ -1,242 +1,89 @@
-<a name="readme-top"></a>
+# 🏆 Tier Maker - 排行分级工具
 
-<!-- PROJECT SHIELDS -->
-<!--
-*** I'm using markdown "reference style" links for readability.
-*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
-*** See the bottom of this document for the declaration of the reference variables
-*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
-*** https://www.markdownguide.org/basic-syntax/#reference-style-links
--->
+一个类似 tier-maker 网站的网页应用，用于为一系列同类元素进行排行分级。
 
-[![Contributors][contributors-shield]][contributors-url]
-[![Forks][forks-shield]][forks-url]
-[![Stargazers][stars-shield]][stars-url]
-[![Issues][issues-shield]][issues-url]
-[![MIT License][license-shield]][license-url]
+## ✨ 主要功能
 
-<!-- PROJECT LOGO -->
-<br />
-<div align="center">
-  <a href="https://github.com/othneildrew/Best-README-Template">
-    <img src="https://icons.iconarchive.com/icons/erix-subyarko/medical/128/Disease-Insect-Covid-Coronavirus-Virus-icon.png" alt="Logo" width="80" height="80">
-  </a>
+- **📁 图片上传**: 支持批量上传图片作为待分级元素
+- **🎯 拖拽分级**: 直观的拖拽操作，将元素拖拽到不同分级槽
+- **⚙️ 自定义分级**: 
+  - 修改分级标签名称
+  - 自定义分级颜色
+  - 添加/删除分级
+- **✏️ 元素管理**: 
+  - 编辑元素名称和描述
+  - 删除不需要的元素
+- **🎨 个性化配置**: 完全自定义的分级样式和布局
 
-  <h3 align="center">Best-README-Template</h3>
+## 🚀 快速开始
 
-  <p align="center">
-    An awesome README template to jumpstart your projects!
-    <br />
-    <br />
-    <a href="https://github.com/othneildrew/Best-README-Template">View Demo</a>
-    ·
-    <a href="https://github.com/othneildrew/Best-README-Template/issues/new?labels=bug&template=bug-report---.md">Report Bug</a>
-    ·
-    <a href="https://github.com/othneildrew/Best-README-Template/issues/new?labels=enhancement&template=feature-request---.md">Request Feature</a>
-  </p>
-</div>
-
-<!-- TABLE OF CONTENTS -->
-<details>
-  <summary>Table of Contents</summary>
-  <ol>
-    <li>
-      <a href="#about-the-project">About The Project</a>
-      <ul>
-        <li><a href="#built-with">Built With</a></li>
-      </ul>
-    </li>
-    <li>
-      <a href="#getting-started">Getting Started</a>
-      <ul>
-        <li><a href="#prerequisites">Prerequisites</a></li>
-        <li><a href="#installation">Installation</a></li>
-      </ul>
-    </li>
-    <li><a href="#usage">Usage</a></li>
-    <li><a href="#roadmap">Roadmap</a></li>
-    <li><a href="#contributing">Contributing</a></li>
-    <li><a href="#license">License</a></li>
-    <li><a href="#contact">Contact</a></li>
-    <li><a href="#acknowledgments">Acknowledgments</a></li>
-  </ol>
-</details>
-
-<!-- ABOUT THE PROJECT -->
-
-## About The Project
-
-There are many great README templates available on GitHub; however, I didn't find one that really suited my needs so I created this enhanced one. I want to create a README template so amazing that it'll be the last one you ever need -- I think this is it.
-
-Here's why:
-
-- Your time should be focused on creating something amazing. A project that solves a problem and helps others
-- You shouldn't be doing the same tasks over and over like creating a README from scratch
-- You should implement DRY principles to the rest of your life
-
-Of course, no one template will serve all projects since your needs may be different. So I'll be adding more in the near future. You may also suggest changes by forking this repo and creating a pull request or opening an issue. Thanks to all the people have contributed to expanding this template!
-
-Use the `BLANK_README.md` to get started.
-
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-### Built With
-
-This section should list any major frameworks/libraries used to bootstrap your project. Leave any add-ons/plugins for the acknowledgements section. Here are a few examples.
-
-- [![Next][Next.js]][Next-url]
-- [![React][React.js]][React-url]
-- [![Vue][Vue.js]][Vue-url]
-- [![Angular][Angular.io]][Angular-url]
-- [![Svelte][Svelte.dev]][Svelte-url]
-- [![Laravel][Laravel.com]][Laravel-url]
-- [![Bootstrap][Bootstrap.com]][Bootstrap-url]
-- [![JQuery][JQuery.com]][JQuery-url]
-
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-<!-- GETTING STARTED -->
-
-## Getting Started
-
-This is an example of how you may give instructions on setting up your project locally.
-To get a local copy up and running follow these simple example steps.
-
-### Prerequisites
-
-This is an example of how to list things you need to use the software and how to install them.
-
-- npm
-  ```sh
-  npm install npm@latest -g
-  ```
-
-### Installation
-
-_Below is an example of how you can instruct your audience on installing and setting up your app. This template doesn't rely on any external dependencies or services._
-
-1. Get a free API Key at [https://example.com](https://example.com)
-2. Clone the repo
-   ```sh
-   git clone https://github.com/your_username_/Project-Name.git
+1. 克隆项目到本地
+2. 在项目目录中启动本地服务器：
+   ```bash
+   # 使用 Python (推荐)
+   python -m http.server 8000
+   
+   # 或使用 Node.js
+   npx serve .
+   
+   # 或使用任何其他静态文件服务器
    ```
-3. Install NPM packages
-   ```sh
-   npm install
-   ```
-4. Enter your API in `config.js`
-   ```js
-   const API_KEY = "ENTER YOUR API";
-   ```
+3. 在浏览器中打开 `http://localhost:8000`
 
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
+## 📖 使用说明
 
-<!-- USAGE EXAMPLES -->
+### 上传元素
+1. 点击"📁 上传图片"按钮
+2. 选择一个或多个图片文件
+3. 图片将出现在底部的"元素池"中
 
-## Usage
+### 分级操作
+1. 从元素池中拖拽元素到对应的分级行
+2. 元素会自动吸附到分级槽中
+3. 可以在不同分级间重新拖拽调整
 
-Use this space to show useful examples of how a project can be used. Additional screenshots, code examples and demos work well in this space. You may also link to more resources.
+### 自定义分级
+1. 点击"⚙️ 配置分级"按钮
+2. 修改分级标签名称
+3. 选择分级颜色
+4. 添加新分级或删除现有分级
 
-_For more examples, please refer to the [Documentation](https://example.com)_
+### 编辑元素
+1. 悬停在元素上显示操作按钮
+2. 点击"✏️"编辑元素信息
+3. 点击"❌"删除元素
 
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
+## 🛠️ 技术特点
 
-<!-- ROADMAP -->
+- **纯原生技术**: 使用 HTML5、CSS3 和 JavaScript，无需框架
+- **响应式设计**: 支持桌面和移动设备
+- **流畅动画**: 使用 CSS 动画和过渡效果
+- **直观交互**: 拖拽、悬停、点击等丰富的交互方式
+- **本地存储**: 所有数据在浏览器本地处理，无需服务器
 
-## Roadmap
+## 📱 浏览器支持
 
-- [x] Add Changelog
-- [x] Add back to top links
-- [ ] Add Additional Templates w/ Examples
-- [ ] Add "components" document to easily copy & paste sections of the readme
-- [ ] Multi-language Support
-  - [ ] Chinese
-  - [ ] Spanish
+- Chrome 60+
+- Firefox 55+
+- Safari 12+
+- Edge 79+
 
-See the [open issues](https://github.com/othneildrew/Best-README-Template/issues) for a full list of proposed features (and known issues).
+## 🎯 项目结构
 
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
+```
+tier-maker/
+├── index.html          # 主页面
+├── index.js            # 应用逻辑
+├── styles.css          # 样式文件
+├── base.css            # 基础样式
+├── package.json        # 项目配置
+└── README.md           # 说明文档
+```
 
-<!-- CONTRIBUTING -->
+## 🤝 贡献
 
-## Contributing
+欢迎提交 Issue 和 Pull Request 来改进这个项目！
 
-Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+## 📄 许可证
 
-If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
-Don't forget to give the project a star! Thanks again!
-
-1. Fork the Project
-2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
-3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
-4. Push to the Branch (`git push origin feature/AmazingFeature`)
-5. Open a Pull Request
-
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-<!-- LICENSE -->
-
-## License
-
-Distributed under the MIT License. See `LICENSE.txt` for more information.
-
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-<!-- CONTACT -->
-
-## Contact
-
-Your Name - [@your_twitter](https://twitter.com/your_username) - email@example.com
-
-Project Link: [https://github.com/your_username/repo_name](https://github.com/your_username/repo_name)
-
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-<!-- ACKNOWLEDGMENTS -->
-
-## Acknowledgments
-
-Use this space to list resources you find helpful and would like to give credit to. I've included a few of my favorites to kick things off!
-
-- [Choose an Open Source License](https://choosealicense.com)
-- [GitHub Emoji Cheat Sheet](https://www.webpagefx.com/tools/emoji-cheat-sheet)
-- [Malven's Flexbox Cheatsheet](https://flexbox.malven.co/)
-- [Malven's Grid Cheatsheet](https://grid.malven.co/)
-- [Img Shields](https://shields.io)
-- [GitHub Pages](https://pages.github.com)
-- [Font Awesome](https://fontawesome.com)
-- [React Icons](https://react-icons.github.io/react-icons/search)
-
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-<!-- MARKDOWN LINKS & IMAGES -->
-<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
-
-[contributors-shield]: https://img.shields.io/github/contributors/othneildrew/Best-README-Template.svg?style=for-the-badge
-[contributors-url]: https://github.com/othneildrew/Best-README-Template/graphs/contributors
-[forks-shield]: https://img.shields.io/github/forks/othneildrew/Best-README-Template.svg?style=for-the-badge
-[forks-url]: https://github.com/othneildrew/Best-README-Template/network/members
-[stars-shield]: https://img.shields.io/github/stars/othneildrew/Best-README-Template.svg?style=for-the-badge
-[stars-url]: https://github.com/othneildrew/Best-README-Template/stargazers
-[issues-shield]: https://img.shields.io/github/issues/othneildrew/Best-README-Template.svg?style=for-the-badge
-[issues-url]: https://github.com/othneildrew/Best-README-Template/issues
-[license-shield]: https://img.shields.io/github/license/othneildrew/Best-README-Template.svg?style=for-the-badge
-[license-url]: https://github.com/othneildrew/Best-README-Template/blob/master/LICENSE.txt
-[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
-[product-screenshot]: images/screenshot.png
-[Next.js]: https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white
-[Next-url]: https://nextjs.org/
-[React.js]: https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB
-[React-url]: https://reactjs.org/
-[Vue.js]: https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vuedotjs&logoColor=4FC08D
-[Vue-url]: https://vuejs.org/
-[Angular.io]: https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white
-[Angular-url]: https://angular.io/
-[Svelte.dev]: https://img.shields.io/badge/Svelte-4A4A55?style=for-the-badge&logo=svelte&logoColor=FF3E00
-[Svelte-url]: https://svelte.dev/
-[Laravel.com]: https://img.shields.io/badge/Laravel-FF2D20?style=for-the-badge&logo=laravel&logoColor=white
-[Laravel-url]: https://laravel.com
-[Bootstrap.com]: https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white
-[Bootstrap-url]: https://getbootstrap.com
-[JQuery.com]: https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white
-[JQuery-url]: https://jquery.com
+MIT License
--- index.js
+++ index.js
@@ -1,15 +1,478 @@
-import { LitElement, html, css } from 'https://unpkg.com/lit@latest?module';
+// Tier Maker 应用主文件
+class TierMaker {
+  constructor() {
+    this.elements = [];
+    this.tiers = [
+      { id: 'S', label: 'S', color: '#ff7f7f', elements: [] },
+      { id: 'A', label: 'A', color: '#ffbf7f', elements: [] },
+      { id: 'B', label: 'B', color: '#ffdf7f', elements: [] },
+      { id: 'C', label: 'C', color: '#ffff7f', elements: [] },
+      { id: 'D', label: 'D', color: '#bfff7f', elements: [] }
+    ];
+    this.draggedElement = null;
+    this.nextElementId = 1;
 
-class MyApp extends LitElement {
-  static styles = css`
-    h1 {
-      color: red;
-    }
-  `
+    this.init();
+  }
+
+  init() {
+    this.render();
+    this.bindEvents();
+  }
+
   render() {
-    return html`
-    <h1>Hello</h1>
+    const app = document.getElementById('app');
+    app.innerHTML = `
+      <div class="app">
+        <div class="header">
+          <h1>🏆 Tier Maker</h1>
+        </div>
+        <div class="main-content">
+          <div class="controls">
+            <div class="file-input-wrapper">
+              <input type="file" id="imageInput" class="file-input" accept="image/*" multiple>
+              <button class="upload-btn" onclick="document.getElementById('imageInput').click()">
+                📁 上传图片
+              </button>
+            </div>
+            <button class="config-btn" onclick="tierMaker.openConfigModal()">
+              ⚙️ 配置分级
+            </button>
+            <button class="clear-btn" onclick="tierMaker.clearAll()">
+              🗑️ 清空所有
+            </button>
+          </div>
+          
+          <div class="tier-container">
+            ${this.renderTiers()}
+          </div>
+          
+          <div class="elements-pool">
+            <h3>📦 元素池</h3>
+            <div class="elements-container" id="elementsContainer">
+              ${this.renderPoolElements()}
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <!-- 配置模态框 -->
+      <div class="modal" id="configModal">
+        <div class="modal-content">
+          <h3>配置分级</h3>
+          <div id="tierConfigList">
+            ${this.renderTierConfig()}
+          </div>
+          <div class="modal-actions">
+            <button class="btn btn-primary" onclick="tierMaker.addTier()">添加分级</button>
+            <button class="btn btn-secondary" onclick="tierMaker.closeConfigModal()">关闭</button>
+          </div>
+        </div>
+      </div>
+      
+      <!-- 元素编辑模态框 -->
+      <div class="modal" id="elementModal">
+        <div class="modal-content">
+          <h3>编辑元素</h3>
+          <div class="form-group">
+            <label>名称</label>
+            <input type="text" id="elementName" placeholder="输入元素名称">
+          </div>
+          <div class="form-group">
+            <label>描述</label>
+            <textarea id="elementDescription" placeholder="输入元素描述" rows="3"></textarea>
+          </div>
+          <div class="modal-actions">
+            <button class="btn btn-primary" onclick="tierMaker.saveElement()">保存</button>
+            <button class="btn btn-secondary" onclick="tierMaker.closeElementModal()">取消</button>
+          </div>
+        </div>
+      </div>
+      
+      <div class="drag-indicator" id="dragIndicator"></div>
     `;
   }
+
+  renderTiers() {
+    return this.tiers.map(tier => `
+      <div class="tier-row">
+        <div class="tier-label" style="background-color: ${tier.color}" 
+             onclick="tierMaker.editTierLabel('${tier.id}')" 
+             id="tierLabel_${tier.id}">
+          ${tier.label}
+        </div>
+        <div class="tier-content" 
+             data-tier-id="${tier.id}"
+             ondrop="tierMaker.handleDrop(event)"
+             ondragover="tierMaker.handleDragOver(event)"
+             ondragleave="tierMaker.handleDragLeave(event)">
+          ${tier.elements.map(element => this.renderElement(element)).join('')}
+        </div>
+      </div>
+    `).join('');
+  }
+
+  renderPoolElements() {
+    const poolElements = this.elements.filter(el => !this.isElementInTier(el.id));
+    return poolElements.map(element => this.renderElement(element)).join('');
+  }
+
+  renderElement(element) {
+    return `
+      <div class="element" 
+           draggable="true" 
+           data-element-id="${element.id}"
+           ondragstart="tierMaker.handleDragStart(event)"
+           ondragend="tierMaker.handleDragEnd(event)">
+        <img src="${element.src}" alt="${element.name}">
+        <div class="element-info">${element.name}</div>
+        <div class="element-actions">
+          <button class="element-action edit-btn" onclick="tierMaker.editElement('${element.id}')">✏️</button>
+          <button class="element-action delete-btn" onclick="tierMaker.deleteElement('${element.id}')">❌</button>
+        </div>
+      </div>
+    `;
+  }
+
+  renderTierConfig() {
+    return this.tiers.map(tier => `
+      <div class="form-group">
+        <label>分级 ${tier.label}</label>
+        <div style="display: flex; gap: 10px; align-items: center;">
+          <input type="text" value="${tier.label}" 
+                 onchange="tierMaker.updateTierLabel('${tier.id}', this.value)"
+                 style="flex: 1;">
+          <input type="color" value="${tier.color}" 
+                 onchange="tierMaker.updateTierColor('${tier.id}', this.value)"
+                 class="color-input">
+          <button class="btn btn-secondary" onclick="tierMaker.deleteTier('${tier.id}')"
+                  style="padding: 8px 12px;">删除</button>
+        </div>
+      </div>
+    `).join('');
+  }
+
+  bindEvents() {
+    // 文件上传
+    document.getElementById('imageInput').addEventListener('change', (e) => {
+      this.handleFileUpload(e);
+    });
+
+    // 元素池拖拽
+    const elementsContainer = document.getElementById('elementsContainer');
+    elementsContainer.addEventListener('drop', (e) => this.handlePoolDrop(e));
+    elementsContainer.addEventListener('dragover', (e) => this.handleDragOver(e));
+    elementsContainer.addEventListener('dragleave', (e) => this.handleDragLeave(e));
+
+    // 点击模态框外部关闭
+    document.addEventListener('click', (e) => {
+      if (e.target.classList.contains('modal')) {
+        this.closeAllModals();
+      }
+    });
+  }
+
+  handleFileUpload(event) {
+    const files = Array.from(event.target.files);
+
+    files.forEach(file => {
+      if (file.type.startsWith('image/')) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const element = {
+            id: this.nextElementId++,
+            name: file.name.split('.')[0],
+            description: '',
+            src: e.target.result
+          };
+          this.elements.push(element);
+          this.updateElementsPool();
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+
+    // 清空文件输入
+    event.target.value = '';
+  }
+
+  handleDragStart(event) {
+    const elementId = event.target.closest('.element').dataset.elementId;
+    this.draggedElement = elementId;
+    event.target.closest('.element').classList.add('dragging');
+
+    // 显示拖拽指示器
+    const indicator = document.getElementById('dragIndicator');
+    indicator.textContent = '拖拽到分级区域';
+    indicator.classList.add('show');
+
+    event.dataTransfer.effectAllowed = 'move';
+  }
+
+  handleDragEnd(event) {
+    event.target.closest('.element').classList.remove('dragging');
+    this.draggedElement = null;
+
+    // 隐藏拖拽指示器
+    const indicator = document.getElementById('dragIndicator');
+    indicator.classList.remove('show');
+
+    // 移除所有拖拽样式
+    document.querySelectorAll('.drag-over').forEach(el => {
+      el.classList.remove('drag-over');
+    });
+  }
+
+  handleDragOver(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+    event.currentTarget.classList.add('drag-over');
+
+    // 更新拖拽指示器位置
+    const indicator = document.getElementById('dragIndicator');
+    indicator.style.left = event.clientX + 10 + 'px';
+    indicator.style.top = event.clientY - 30 + 'px';
+  }
+
+  handleDragLeave(event) {
+    if (!event.currentTarget.contains(event.relatedTarget)) {
+      event.currentTarget.classList.remove('drag-over');
+    }
+  }
+
+  handleDrop(event) {
+    event.preventDefault();
+    event.currentTarget.classList.remove('drag-over');
+
+    if (!this.draggedElement) return;
+
+    const tierId = event.currentTarget.dataset.tierId;
+    this.moveElementToTier(this.draggedElement, tierId);
+  }
+
+  handlePoolDrop(event) {
+    event.preventDefault();
+    event.currentTarget.classList.remove('drag-over');
+
+    if (!this.draggedElement) return;
+
+    this.moveElementToPool(this.draggedElement);
+  }
+
+  moveElementToTier(elementId, tierId) {
+    // 从所有分级中移除元素
+    this.tiers.forEach(tier => {
+      tier.elements = tier.elements.filter(id => id !== elementId);
+    });
+
+    // 添加到目标分级
+    const targetTier = this.tiers.find(tier => tier.id === tierId);
+    if (targetTier) {
+      targetTier.elements.push(elementId);
+    }
+
+    this.updateDisplay();
+  }
+
+  moveElementToPool(elementId) {
+    // 从所有分级中移除元素
+    this.tiers.forEach(tier => {
+      tier.elements = tier.elements.filter(id => id !== elementId);
+    });
+
+    this.updateDisplay();
+  }
+
+  isElementInTier(elementId) {
+    return this.tiers.some(tier => tier.elements.includes(elementId));
+  }
+
+  updateDisplay() {
+    // 更新分级区域
+    this.tiers.forEach(tier => {
+      const tierContent = document.querySelector(`[data-tier-id="${tier.id}"]`);
+      if (tierContent) {
+        tierContent.innerHTML = tier.elements.map(elementId => {
+          const element = this.elements.find(el => el.id == elementId);
+          return element ? this.renderElement(element) : '';
+        }).join('');
+      }
+    });
+
+    // 更新元素池
+    this.updateElementsPool();
+  }
+
+  updateElementsPool() {
+    const container = document.getElementById('elementsContainer');
+    if (container) {
+      container.innerHTML = this.renderPoolElements();
+    }
+  }
+
+  editTierLabel(tierId) {
+    const labelElement = document.getElementById(`tierLabel_${tierId}`);
+    const tier = this.tiers.find(t => t.id === tierId);
+
+    if (!tier || labelElement.classList.contains('editing')) return;
+
+    labelElement.classList.add('editing');
+    const currentLabel = tier.label;
+
+    labelElement.innerHTML = `<input type="text" value="${currentLabel}" onblur="tierMaker.saveTierLabel('${tierId}', this.value)" onkeypress="if(event.key==='Enter') this.blur()">`;
+    labelElement.querySelector('input').focus();
+    labelElement.querySelector('input').select();
+  }
+
+  saveTierLabel(tierId, newLabel) {
+    const tier = this.tiers.find(t => t.id === tierId);
+    const labelElement = document.getElementById(`tierLabel_${tierId}`);
+
+    if (tier && newLabel.trim()) {
+      tier.label = newLabel.trim();
+    }
+
+    labelElement.classList.remove('editing');
+    labelElement.innerHTML = tier.label;
+  }
+
+  editElement(elementId) {
+    const element = this.elements.find(el => el.id == elementId);
+    if (!element) return;
+
+    this.currentEditingElement = element;
+    document.getElementById('elementName').value = element.name;
+    document.getElementById('elementDescription').value = element.description;
+    document.getElementById('elementModal').classList.add('show');
+  }
+
+  saveElement() {
+    if (!this.currentEditingElement) return;
+
+    const name = document.getElementById('elementName').value.trim();
+    const description = document.getElementById('elementDescription').value.trim();
+
+    if (name) {
+      this.currentEditingElement.name = name;
+      this.currentEditingElement.description = description;
+      this.updateDisplay();
+    }
+
+    this.closeElementModal();
+  }
+
+  deleteElement(elementId) {
+    if (confirm('确定要删除这个元素吗？')) {
+      // 从元素列表中移除
+      this.elements = this.elements.filter(el => el.id != elementId);
+
+      // 从所有分级中移除
+      this.tiers.forEach(tier => {
+        tier.elements = tier.elements.filter(id => id != elementId);
+      });
+
+      this.updateDisplay();
+    }
+  }
+
+  openConfigModal() {
+    document.getElementById('configModal').classList.add('show');
+    // 重新渲染配置列表
+    document.getElementById('tierConfigList').innerHTML = this.renderTierConfig();
+  }
+
+  closeConfigModal() {
+    document.getElementById('configModal').classList.remove('show');
+  }
+
+  closeElementModal() {
+    document.getElementById('elementModal').classList.remove('show');
+    this.currentEditingElement = null;
+  }
+
+  closeAllModals() {
+    document.querySelectorAll('.modal').forEach(modal => {
+      modal.classList.remove('show');
+    });
+  }
+
+  updateTierLabel(tierId, newLabel) {
+    const tier = this.tiers.find(t => t.id === tierId);
+    if (tier && newLabel.trim()) {
+      tier.label = newLabel.trim();
+      // 更新显示
+      const labelElement = document.getElementById(`tierLabel_${tierId}`);
+      if (labelElement) {
+        labelElement.textContent = tier.label;
+      }
+    }
+  }
+
+  updateTierColor(tierId, newColor) {
+    const tier = this.tiers.find(t => t.id === tierId);
+    if (tier) {
+      tier.color = newColor;
+      // 更新显示
+      const labelElement = document.getElementById(`tierLabel_${tierId}`);
+      if (labelElement) {
+        labelElement.style.backgroundColor = newColor;
+      }
+    }
+  }
+
+  addTier() {
+    const newId = 'T' + Date.now();
+    const newTier = {
+      id: newId,
+      label: '新分级',
+      color: '#' + Math.floor(Math.random() * 16777215).toString(16),
+      elements: []
+    };
+
+    this.tiers.push(newTier);
+
+    // 重新渲染整个应用
+    this.render();
+    this.bindEvents();
+
+    // 重新打开配置模态框
+    this.openConfigModal();
+  }
+
+  deleteTier(tierId) {
+    if (this.tiers.length <= 1) {
+      alert('至少需要保留一个分级！');
+      return;
+    }
+
+    if (confirm('确定要删除这个分级吗？分级中的元素将移回元素池。')) {
+      const tier = this.tiers.find(t => t.id === tierId);
+      if (tier && tier.elements.length > 0) {
+        // 将分级中的元素移回元素池（实际上就是从分级中移除）
+      }
+
+      this.tiers = this.tiers.filter(t => t.id !== tierId);
+
+      // 重新渲染
+      this.render();
+      this.bindEvents();
+      this.openConfigModal();
+    }
+  }
+
+  clearAll() {
+    if (confirm('确定要清空所有元素和分级设置吗？此操作不可撤销！')) {
+      this.elements = [];
+      this.tiers.forEach(tier => {
+        tier.elements = [];
+      });
+      this.updateDisplay();
+    }
+  }
 }
-customElements.define('my-app', MyApp);
+
+// 初始化应用
+const tierMaker = new TierMaker();
+
+// 导出到全局作用域以便HTML中的事件处理器使用
+window.tierMaker = tierMaker;
